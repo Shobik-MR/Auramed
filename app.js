e36aa50ff345f9bdf68e89fadfe94a5225a17094
Shobik-don <shobikshobik504@gmail.com>
2026-01-30 21:41:36 +0530

    login page

---
+++ app.js
@@ -0,0 +1,11 @@
+document.querySelector(".green").addEventListener("click", () => {
+  alert("Redirecting to hospital search...");
+});
+
+document.querySelector(".orange").addEventListener("click", () => {
+  alert("Calling ambulance services...");
+});
+
+document.querySelector(".white").addEventListener("click", () => {
+  alert("Opening hospital dashboard...");
+});
